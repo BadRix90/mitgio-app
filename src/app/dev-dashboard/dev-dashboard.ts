3c60a8d962402a9c05851873cbaef1d9c1439331
Kay <118884667+BadRix90@users.noreply.github.com>
2025-08-19 09:50:54 +0200

    delete old testfiles

--- src/app/dev-dashboard/dev-dashboard.ts
+++ src/app/dev-dashboard/dev-dashboard.ts
@@ -2,21 +2,14 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Firestore, collection, addDoc, serverTimestamp } from '@angular/fire/firestore';
 import { TestDbComponent } from "../test-db.component";
-import { OrgTestComponent } from '../org-test/org-test';
-import { MembersTestComponent } from '../members-test/members-test';
-import { MembersListComponent } from '../members-list/members-list';
-import { OrgListComponent } from '../org-list/org-list';
+
 
 @Component({
   selector: 'app-dev-dashboard',
   standalone: true,
   imports: [
     CommonModule,
-    TestDbComponent, 
-    OrgTestComponent, 
-    MembersTestComponent, 
-    MembersListComponent,
-    OrgListComponent
+    TestDbComponent
   ],
   templateUrl: './dev-dashboard.html',
   styleUrls: ['./dev-dashboard.scss']
